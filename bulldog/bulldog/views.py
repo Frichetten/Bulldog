568516cddc273c09d084e352bf10fe782ca3861a
Nicholas Frichette <Frichetten@users.noreply.github.com>
2017-08-19 01:21:02 -0500

    Add files via upload

--- bulldog/bulldog/views.py
+++ bulldog/bulldog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import subprocess
 
+commands = ['ifconfig','ls','echo','pwd','cat','rm']
+
 def homepage(request):
     return render(request, 'index.html')
 
@@ -14,8 +16,20 @@ def shell(request):
     if request.method == "POST":
 	command = request.POST.get("command", None)
 	to_return = "Command : " + command + "\n\n"
-	execu = subprocess.check_output(command, shell=True)
-	to_return += execu
+
+	if validate(command):
+	    execute = subprocess.check_output(command, shell=True)
+	    to_return += execute
+  	elif ";" in command:
+	    to_return += "INVALID COMMAND. I CAUGHT YOU HACKER! ';' CAN BE USED TO EXECUTE MULTIPLE COMMANDS!!"
+	else:
+	    to_return += "INVALID COMMAND. I CAUGHT YOU HACKER!"
+
 	context = {'data': to_return}
 	return render(request, 'shell.html', context)
     return render(request, 'shell.html')
+
+def validate(command):
+    if any(com in command for com in commands) and ";" not in command:
+        return True
+    return False
